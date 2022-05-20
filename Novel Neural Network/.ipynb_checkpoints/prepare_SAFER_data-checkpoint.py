6934cafee38638e668fd62a3cdfd01440276ea6e
js2432 <js2432@cam.ac.uk>
2022-05-20 17:12:47 +0100

    got results for the time savings using prioritisation method in percentage

--- Novel Neural Network/.ipynb_checkpoints/prepare_SAFER_data-checkpoint.py
+++ Novel Neural Network/.ipynb_checkpoints/prepare_SAFER_data-checkpoint.py
@@ -20,34 +20,34 @@ for root, dirs, files in os.walk("F:\\DATA\\JSmith_SAFER_20220310\\raw_data"):
 print('all files found :)')
 
 path_to_metadata_files = "F:\\DATA\\JSmith_SAFER_20220310\\metadata"
-with open(os.path.join(path_to_metadata_files, "feas1"), "rb") as file:   
-    meas_id_list_feas1 = pickle.load(file)
+with open(os.path.join(path_to_metadata_files, "feas2"), "rb") as file:   
+    meas_id_list_feas2 = pickle.load(file)
     
-feas1_filepaths_labelled = []
+feas2_filepaths_labelled = []
 for filepath in tqdm(all_files):
-    if "Feas1" in filepath:
-        if int(filepath.split("saferF1_",2)[1]) in meas_id_list_feas1:
-            feas1_filepaths_labelled.append(filepath)
+    if "Feas2" in filepath:
+        if int(filepath.split("saferF2_",2)[1]) in meas_id_list_feas2:
+            feas2_filepaths_labelled.append(filepath)
 
-af_df = pd.read_csv("F:\\DATA\\JSmith_SAFER_20220310\\raw_data\\Feas1\\files_with_af.csv", names=["filename"])
-filenames_with_af = af_df.filename.tolist()
+# af_df = pd.read_csv("F:\\DATA\\JSmith_SAFER_20220310\\raw_data\\Feas2\\files_with_af.csv", names=["filename"])
+# filenames_with_af = af_df.filename.tolist()
 
 
 #define filter parameters
 order = 6
 fs = 30.0  # sampling frequency
 
-# feas 1
-feas1_path_to_folder = "F:\\DATA\\JSmith_SAFER_20220310\\preprocessed_labelled_data\\Feas1"
-feas1_already_processed_files = [files for root, dirs, files in os.walk(feas1_path_to_folder)][0]
-feas1_processed_file_as_string = ''.join(item for item in feas1_already_processed_files)
+# feas 2
+feas2_path_to_folder = "F:\\DATA\\JSmith_SAFER_20220310\\preprocessed_labelled_data\\Feas2"
+feas2_already_processed_files = [files for root, dirs, files in os.walk(feas2_path_to_folder)][0]
+feas2_processed_file_as_string = ''.join(item for item in feas2_already_processed_files)
 
-for test_file in tqdm(feas1_filepaths_labelled):
+for test_file in tqdm(feas2_filepaths_labelled):
     
     # check not repeating process for already used file
     test_file_name = test_file.split("\\")[-1]    
     
-    if test_file_name not in feas1_processed_file_as_string:
+    if test_file_name not in feas2_processed_file_as_string:
         
         #extract the sample 
         record = wfdb.rdrecord(test_file)
@@ -62,7 +62,7 @@ for test_file in tqdm(feas1_filepaths_labelled):
                         
         #save this preprocessed sample
         filename = test_file.split("\\")[-1]
-        npy_path = os.path.join(feas1_path_to_folder, filename)
+        npy_path = os.path.join(feas2_path_to_folder, filename)
         try:
             np.save(npy_path, signal_normalised)
         except:
